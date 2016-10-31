75cdc320f2014c6e51e13d9b8651e8854fca1b7c
johodges <johodges@vt.edu>
2016-10-31 10:26:22 -0400

    Updated panel.sh so we can auto-start gazebo.

--- mbzirc_c2_state/bin/mbzirc_simulation_state_machine_2.py
+++ mbzirc_c2_state/bin/mbzirc_simulation_state_machine_2.py
@@ -24,7 +24,7 @@ class FindBoard(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo('Searching for board')
-	
+	#a0 = subprocess.Popen("rosrun husky_control stow_ur5", shell=True)
 	a = subprocess.Popen("rosrun mbzirc_c2_auto findbox.py", shell=True)
 	b = subprocess.Popen("rosrun mbzirc_c2_auto autonomous.py", shell=True)
 
