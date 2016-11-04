07e53b37430601d01f6d6d47bb54c106d186f431
Alan Lattimer <alattimer@jensenhughes.com>
2016-11-04 15:49:11 -0400

    Removed call to stow the arm since it now spawns already stowed.

--- mbzirc_c2_state/bin/mbzirc_simulation_state_machine_2.py
+++ mbzirc_c2_state/bin/mbzirc_simulation_state_machine_2.py
@@ -24,9 +24,9 @@ class FindBoard(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo('Searching for board')
-        rospy.sleep(0.1)
-	a0 = subprocess.Popen("rosrun husky_control stow_ur5", shell=True)
-        rospy.sleep(10)
+ #        rospy.sleep(0.1)
+	# a0 = subprocess.Popen("rosrun husky_control stow_ur5", shell=True)
+        rospy.sleep(1)
 	a = subprocess.Popen("rosrun mbzirc_c2_auto findbox.py", shell=True)
 	b = subprocess.Popen("rosrun mbzirc_c2_auto autonomous.py", shell=True)
 
