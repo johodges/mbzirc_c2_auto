b612ef7cb0964bb8628f871a41870b09d165519a
Alan Lattimer <alattimer@jensenhughes.com>
2016-11-04 14:56:57 -0400

    Fixed initial position of the robot

--- mbzirc_c2_state/bin/mbzirc_simulation_state_machine_2.py
+++ mbzirc_c2_state/bin/mbzirc_simulation_state_machine_2.py
@@ -24,6 +24,7 @@ class FindBoard(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo('Searching for board')
+        rospy.sleep(0.1)
 	a0 = subprocess.Popen("rosrun husky_control stow_ur5", shell=True)
         rospy.sleep(10)
 	a = subprocess.Popen("rosrun mbzirc_c2_auto findbox.py", shell=True)
