ac6a2ec133b637a5e5e8dda051149edd130e6bfa
johodges <johodges@vt.edu>
2016-11-11 18:04:13 -0500

    Add feedback to determine if arm plan was achieved.

--- mbzirc_grasping/scripts/move_arm_param.py
+++ mbzirc_grasping/scripts/move_arm_param.py
@@ -19,73 +19,101 @@ import sys
 import moveit_commander
 from geometry_msgs.msg import PoseArray, PoseStamped
 from sensor_msgs.msg import JointState
+from control_msgs.msg import FollowJointTrajectoryActionResult
+from actionlib_msgs.msg import *
 import roslib
 roslib.load_manifest("rosparam")
 import rosparam
 
+class move_arm_param():
+    def __init__(self):
+        # Name this node, it must be unique
+	rospy.init_node('move_arm', anonymous=False)
+        rospy.on_shutdown(self.shutdown) # Set rospy to execute a shutdown function when exiting
+        self.flag = 0
+        self.ct = 0
 
-def main():
-    rospy.init_node("move_arm", anonymous=False)
+        # Set up ROS subscriber callback routines
+        rospy.Subscriber("/move_group/status", GoalStatusArray, self.callback, queue_size=1)
 
-    rospy.set_param('arm_prefix', 'ur5_arm_')
-    rospy.set_param('reference_frame', '/base_link')
+        rospy.set_param('arm_prefix', 'ur5_arm_')
+        rospy.set_param('reference_frame', '/base_link')
+        rospy.loginfo("Moving arm to desired position")
 
-    rospy.loginfo("Moving arm to desired position")
+        # Initialize the move_group API
+        moveit_commander.roscpp_initialize(sys.argv)
 
-    # Initialize the move_group API
-    moveit_commander.roscpp_initialize(sys.argv)
+        # Initialize the move group for the ur5_arm
+        arm = moveit_commander.MoveGroupCommander("ur5_arm")
 
-    # Initialize the move group for the ur5_arm
-    arm = moveit_commander.MoveGroupCommander("ur5_arm")
+        # Get the name of the end-effector link
+        end_effector_link = arm.get_end_effector_link()
+        rospy.loginfo(end_effector_link)
 
-    # Get the name of the end-effector link
-    end_effector_link = arm.get_end_effector_link()
-    rospy.loginfo(end_effector_link)
+        # Initialize Necessary Variables
+        reference_frame = rospy.get_param("~reference_frame", "/base_link")
+        #reference_frame = "ee_link"
 
-    # Initialize Necessary Variables
-    reference_frame = rospy.get_param("~reference_frame", "/base_link")
-    #reference_frame = "ee_link"
+        # Set the ur5_arm reference frame accordingly
+        arm.set_pose_reference_frame(reference_frame)
 
-    # Set the ur5_arm reference frame accordingly
-    arm.set_pose_reference_frame(reference_frame)
+        # Allow replanning to increase the odds of a solution
+        arm.allow_replanning(True)
 
-    # Allow replanning to increase the odds of a solution
-    arm.allow_replanning(True)
+        # Allow some leeway in position (meters) and orientation (radians)
+        arm.set_goal_position_tolerance(0.01)
+        arm.set_goal_orientation_tolerance(0.01)
 
-    # Allow some leeway in position (meters) and orientation (radians)
-    arm.set_goal_position_tolerance(0.01)
-    arm.set_goal_orientation_tolerance(0.01)
+        # Set the target pose from the input
+        self.target_pose = PoseStamped()
+        self.target_pose.header.frame_id = reference_frame
+        self.target_pose.header.stamp = rospy.Time.now()
+        ee_pos = rospy.get_param('ee_position')
+        self.target_pose.pose.position.x = ee_pos[0]
+        self.target_pose.pose.position.y = ee_pos[1]
+        self.target_pose.pose.position.z = ee_pos[2]
 
-    # Set the target pose from the input
-    target_pose = PoseStamped()
-    target_pose.header.frame_id = reference_frame
-    target_pose.header.stamp = rospy.Time.now()
-    ee_pos = rospy.get_param('ee_position')
-    target_pose.pose.position.x = ee_pos[0]
-    target_pose.pose.position.y = ee_pos[1]
-    target_pose.pose.position.z = ee_pos[2]
+        # Set the start state to the current state
+        arm.set_start_state_to_current_state()
 
-    # Set the start state to the current state
-    arm.set_start_state_to_current_state()
-    rospy.loginfo(arm.get_start_state)
+        # Set the goal pose of the end effector to the stored pose
+        arm.set_pose_target(self.target_pose, end_effector_link)
 
-    # Set the goal pose of the end effector to the stored pose
-    arm.set_pose_target(target_pose, end_effector_link)
+        # Plan the trajectory to the goal
+        traj = arm.plan()
 
-    # Plan the trajectory to the goal
-    traj = arm.plan()
+        if traj is not None:
+            # Execute the planned trajectory
+            arm.execute(traj)
 
-    if traj is not None:
-        # Execute the planned trajectory
-        arm.execute(traj)
+            # Pause for a second
+            rospy.sleep(1.0)
+            self.flag = 1
 
-        # Pause for a second
-        rospy.sleep(5.0)
+    # The shutdown function is used to cancel any move_base goals when this node is killed
+    def shutdown(self):
+        rospy.loginfo("Stopping the robot...")
 
-        rospy.loginfo("Successfully moved")
-
-    else:
-        rospy.loginfo("Unable to reach")
+    # callback_feedback is used to store the feedback topic into the class to be
+    # referenced by the other callback routines.
+    def callback(self, data):
+        if self.flag == 1:
+            self.status = data.status_list[0].status
+            
+            if self.status == 3:
+                rospy.sleep(5)
+                rospy.set_param('move_arm_status','success')
+                self.flag = 2
+            else:
+                if self.ct > 10:
+                    rospy.set_param('move_arm_status','failure')
+                    self.flag = 2
+                else:
+                    self.ct = self.ct + 1
+                    rospy.sleep(0.5)
+        if self.flag == 2:
+            rospy.signal_shutdown('Ending node.')
 
 if __name__ == '__main__':
-    main()
+    move_arm_param()
+    rospy.spin()
