Fixed rotation mapping

map_reader/scripts/read_amcl.py
@@ -4,12 +4,14 @@ import rospy
 import yaml
 import geometry_msgs.msg
 from geometry_msgs.msg import PoseWithCovarianceStamped
-import math
+import tf
 
 def callback(data):
 	currentX = data.pose.pose.position.x
 	currentY = data.pose.pose.position.y
-	currentR = data.pose.pose.orientation.z * math.pi
+	quart = (data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w)
+	euler = tf.transformations.euler_from_quaternion(quart)
+	currentR = euler[2]
 
 	rospy.set_param("/currentRobotX", currentX)
 	rospy.set_param("/currentRobotY", currentY)

map_reader/scripts/read_odom.py
@@ -5,7 +5,6 @@ import yaml
 import geometry_msgs.msg
 from geometry_msgs.msg import PoseWithCovarianceStamped
 import tf
-import math
 
 def main():
 	rospy.init_node('odom_reader', anonymous=True)
@@ -36,10 +35,13 @@ def main():
  			(trans, rotate) = listener.lookupTransform('/odom', '/base_link', rospy.Time(0))
  		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
  			continue
+
+		print type(rotate)
+		euler = tf.transformations.euler_from_quaternion(rotate)
  		
  		roboX = trans[0]
  		roboY = trans[1]
-		roboR = rotate[2] * math.pi
+		roboR = euler[2]
 		print roboX
 		print roboY
 		print roboR